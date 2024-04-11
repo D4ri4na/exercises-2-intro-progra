d250b7f241dea7a8d980d02b1deedf836d415dac
D4ri4na <159303073+D4ri4na@users.noreply.github.com>
2024-04-10 22:50:05 -0400

    hasta mañana 17

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -201,7 +201,28 @@ void exercise_16(int divident, int divider) {
 }
 
 void exercise_17(int n) {
-  // TODO: YOUR CODE HERE
+   if (n != 0 && n != 1 && n !=4) {
+        for (int i=2 ; i < n/2;i++){
+            if (n % i == 0){
+                if (n == i){
+                    for (int i = 2; i<=n;i++){
+                        cout<<i<<endl;
+                        //if (primo(i))
+                    }
+                    }
+                 else{
+        cout<<"es falsa"<<endl;
+        break;
+                }  
+                //for (int i=2; i<=n;i++){
+                       // cout <<i<<" ";//primo = true;
+                }
+    else{
+        cout<<"es falsa"<<endl;
+        break;
+                }
+                }
+                }
 }
 
 void exercise_18_19(int debut, int fin) {
